03884745452b8d5ad1fd3bc666e923af19ca3a30
RainStormTM <112580407+RainStormTM@users.noreply.github.com>
2022-09-02 00:07:56 +0100

    a
    a

---
+++ Assets/Scripts/UI/UiManagerScript.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManagerScript : MonoBehaviour
+{
+    [SerializeField] GameObject PressAnyKeyGO; //UI First Menu
+    [SerializeField] GameObject MainMenuGO;
+    [SerializeField] GameObject OptionMenu;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuGO.SetActive(false);
+        OptionMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey && PressAnyKeyGO.activeInHierarchy)
+        {
+            PressAnyKeyGO.SetActive(false);
+            MainMenuGO.SetActive(true);
+        }
+    }
+    void MenuHandler()
+    {
+
+    }
+    public void Continue()
+    {
+
+    }
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Options()
+    {
+        MainMenuGO.SetActive(false);
+        OptionMenu.SetActive(true);
+    }
+    public void Quit()
+    {
+        Quit();
+    } public void AnyKey()
+    {
+        
+    }
+    public void Return()
+    {
+        OptionMenu.SetActive(false);
+        MainMenuGO.SetActive(true);
+    }
+}
